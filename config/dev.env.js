48bb7e5c81be41acf99f348c3885d45a9768a730
HappyWjl <820155406@qq.com>
2018-06-06 15:30:50 +0800

    修改数据库连接地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_HOST: '"http://localhost:8888/learn"'
+  API_HOST: '"http://101.200.46.131:8888/learn"'
 })
