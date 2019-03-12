698a476985f4cef427c37f4c1eed6f08a7367186
wangjiale <wangjiale@shuidihuzhu.com>
2019-03-12 14:02:06 +0800

    修改  适配

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import VueQuillEditor from 'vue-quill-editor'
+import 'lib-flexible'
 
+import '../static/js/fontSize.js'
 import '../static/css/theme-green/index.css'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 Vue.use(VueQuillEditor)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
